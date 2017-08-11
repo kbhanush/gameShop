3784c82bc8a48f513dca7885f3832db981ac9850
Kris Bhanushali <kris.bhanushali@oracle.com>
2017-08-11 11:30:04 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ mvn install:install-file -DgroupId=com.oracle.jdbc -DartifactId=ojdbc7 -Dversion
 mvn install:install-file -DgroupId=javax.servlet -DartifactId=javax.servlet.servlet-api -Dversion=2.5 -Dpackaging=jar -Dfile=javax.servlet.javax.servlet-api.jar
 mvn install:install-file -DgroupId=javax.servlet.jsp -DartifactId=javax.servlet.jsp-api -Dversion=2.1 -Dpackaging=jar -Dfile=javax.servlet.jsp.javax.servlet.jsp-api.jar
 mvn clean install
-mvn flyway:migrate -Ddb.ip=129.157.186.185 -Ddb.name=PDB1.usoracle52544.oraclecloud.internal -Dflyway.user=system -Dflyway.password=Temp123#
-
+#mvn flyway:migrate -Ddb.ip=129.157.186.185 -Ddb.name=PDB1.usoracle52544.oraclecloud.internal -Dflyway.user=system -Dflyway.password=Temp123#
+mvn flyway:migrate -Ddb.ip=129.157.186.185 -Ddb.name=PDB1.usoracle52544.oraclecloud.internal -Dflyway.user=WEBAPP2 -Dflyway.password=WEBAPP2
