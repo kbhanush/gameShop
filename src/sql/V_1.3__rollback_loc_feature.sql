34b11c63cf0ea39b5f5ea9e7256ba566fd49d47a
Kris Bhanushali <kris.bhanushali@oracle.com>
2017-08-12 15:24:43 -0700

    rollback

---
+++ src/sql/V_1.3__rollback_loc_feature.sql
@@ -0,0 +1,24 @@
+
+Drop table DeliveryLocations;
+
+alter table order_details
+drop column order_loc_id;
+
+alter table order_details
+drop constraint fk_loc;
+
+CREATE OR REPLACE EDITIONABLE PROCEDURE "ADD_ORDER" ( user_id IN NUMBER, user_realname IN VARCHAR2, user_address IN VARCHAR2, user_phone In NUMBER, user_email IN VARCHAR2, order_price IN NUMBER)
+IS
+BEGIN
+  INSERT INTO order_details(order_id, user_id, status, shippers_name, address, MOBILE_NUMBER,SHIPPERS_EMAIL,ORDERED_ON,TOTAL_ORDER_PRICE) VALUES(
+    SYS_GUID(),
+    user_id,
+    'paid',
+    user_realname,
+    user_address,
+    user_phone,
+    user_email,
+    CURRENT_DATE,
+    order_price
+  );
+END;
