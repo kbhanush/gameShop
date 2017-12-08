dc36cad05141447b0fedec4904a14adb1c657715
Kris Bhanushali <kbhanushali@Kriss-MacBook-Pro.local>
2017-12-08 06:43:45 +0530

    maven to 1.6

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 mvn install:install-file -DgroupId=com.oracle.jdbc -DartifactId=ojdbc7 -Dversion=12.1.0.2 -Dpackaging=jar -Dfile=ojdbc7.jar
-mvn clean package -Ddb.ip=129.150.86.13 -Ddb.name=PDB1.krisipnetwork.oraclecloud.internal -Ddb.user=WEBAPP2 -Ddb.pass=WEBAPP2
-mvn flyway:migrate -Ddb.ip=129.150.86.13 -Ddb.name=PDB1.krisipnetwork.oraclecloud.internal -Dflyway.user=WEBAPP2 -Dflyway.password=WEBAPP2
+mvn clean package -Ddb.ip=129.150.112.129 -Ddb.name=DevSeed.krisipnetwork.oraclecloud.internal -Ddb.user=WEBAPP2 -Ddb.pass=WEBAPP2
+mvn flyway:migrate -Ddb.ip=129.150.112.129 -Ddb.name=DevSeed.krisipnetwork.oraclecloud.internal -Dflyway.user=WEBAPP2 -Dflyway.password=WEBAPP2
