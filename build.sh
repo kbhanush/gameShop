38ad92227a6ec627e9c18a69e597a5ab04fa772b
Kris Bhanushali <kris.bhanushali@oracle.com>
2018-04-17 20:04:26 -0700

    initial build

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+# modified for IOUG Collaborate 18. Database prodDB on krisipnetwork 
+
 mvn install:install-file -DgroupId=com.oracle.jdbc -DartifactId=ojdbc7 -Dversion=12.1.0.2 -Dpackaging=jar -Dfile=ojdbc7.jar
-mvn clean package -Ddb.ip=129.150.112.129 -Ddb.name=dev1.krisipnetwork.oraclecloud.internal -Ddb.user=WEBAPP2 -Ddb.pass=WEBAPP2
-mvn flyway:migrate -Ddb.ip=129.150.112.129 -Ddb.name=dev1.krisipnetwork.oraclecloud.internal -Dflyway.user=WEBAPP2 -Dflyway.password=WEBAPP2
+mvn clean package -Ddb.ip=129.158.74.201 -Ddb.name=appseed.krisipnetwork.oraclecloud.internal -Ddb.user=WEBAPP2 -Ddb.pass=WEBAPP2
+mvn flyway:migrate -Ddb.ip=129.158.74.201 -Ddb.name=appseed.krisipnetwork.oraclecloud.internal -Dflyway.user=WEBAPP2 -Dflyway.password=WEBAPP2
