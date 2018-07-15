2abb011093eb000c2386478db34269f7fa54eea6
Kris Bhanushali <kris.bhanushali@oracle.com>
2018-07-15 11:20:04 -0700

    loc_feature_enabled

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # modified for IOUG Collaborate 18. Database prodDB on krisipnetwork 
 
 mvn install:install-file -DgroupId=com.oracle.jdbc -DartifactId=ojdbc7 -Dversion=12.1.0.2 -Dpackaging=jar -Dfile=ojdbc7.jar
-mvn clean package -Ddb.ip=129.150.89.253 -Ddb.name=GAMESHOPSEEDDB.596600477.oraclecloud.internal -Ddb.user=WEBAPP2 -Ddb.pass=WEBAPP2
-mvn flyway:migrate -Ddb.ip=129.150.89.253 -Ddb.name=GAMESHOPSEEDDB.596600477.oraclecloud.internal -Dflyway.user=WEBAPP2 -Dflyway.password=WEBAPP2
+mvn clean package -Ddb.ip=129.150.89.253 -Ddb.name=kristestdb.596600477.oraclecloud.internal -Ddb.user=WEBAPP2 -Ddb.pass=WEBAPP2
+mvn flyway:migrate -Ddb.ip=129.150.89.253 -Ddb.name=kristestdb.596600477.oraclecloud.internal -Dflyway.user=WEBAPP2 -Dflyway.password=WEBAPP2
